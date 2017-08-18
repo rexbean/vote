bb15708f3f23419fa149e0954f95e4ea8c3d7da7
carlos <419903711@qq.com>
2017-08-18 23:54:38 +0800

    save

--- middlewares/showList.js
+++ middlewares/showList.js
@@ -4,13 +4,25 @@ exports.showList = function(req, res, next){
     console.log('abcdedf');
     var cql = 'select * from PicObject';
     AV.Query.doCloudQuery(cql).then(function (data) {
+        var list = [];
         for(var i=0; i<data.results.length; i++){
-            var result = data.results[i];
-            var userinfo = result.get('userinfo');
-            var base64 = result.get('picture');
-            console.log(i + " "+ userinfo);
+            var obj = {
+              result : data.results[i],
+              userinfo : data.results[i].get('userinfo'),
+              base64 : data.results[i].get('picture')
+            }
+            list.push(JSON.stringify(obj))
         }
+        res.send({
+          success:true,
+          msg:"ok",
+          data:list
+        })
     },function(error){
-        console.log(error);
+        res.send({
+          success:false,
+          msg:error,
+          data:null
+        })
     });
 }
